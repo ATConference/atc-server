e9cb30a1015aca7a981b50ecee38cb2c25969407
Jaewon Seo <byeol@hotmail.co.kr>
2016-11-14 13:00:00 +0900

    Generate a Spring Boot project

---
+++ src/main/java/kr/ac/sogang/creative/AtcServerApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.sogang.creative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtcServerApplication.class, args);
+	}
+}
